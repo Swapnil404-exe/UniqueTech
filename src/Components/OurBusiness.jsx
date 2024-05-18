46b57c406892e01618a9175a5f9c96e553a0cf41
Swapnil404-exe <swapnildhamija53@gmail.com>
2024-05-19 01:45:54 +0530

    Fully Final

---
+++ src/Components/OurBusiness.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+function OurBusiness() {
+  return (
+    <div>
+        <section id="our-project" className="sections">
+            <div className="container">
+
+                <div className="heading">
+                    <div className="title text-center arrow-left">
+
+                        <h4 className="">Our Business Project</h4>
+                        <img className="hidden-xs" src="./src/assets/images/right-arrow.png" alt="" />
+                    </div>
+                </div>
+
+                <div className="row">
+
+
+                    <div className="main-project text-center">
+
+                        <div className="col-md-3 col-sm-6 col-xs-12">
+                            <div className="project-item">
+                                <div className="project-photo"><img src="./src/assets/images/projects/1.png" alt="" /></div>
+                                <h5>TECHNICAL AID</h5>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 col-xs-12">
+                            <div className="project-item">
+                                <div className="project-photo"><img src="./src/assets/images/projects/2.png" alt="" /></div>
+                                <h5>SECURE ACCESS</h5>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 col-xs-12">
+                            <div className="project-item">
+                                <div className="project-photo"><img src="./src/assets/images/projects/3.png" alt="" /></div>
+                                <h5>MARKET RESERCH</h5>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 col-xs-12">
+                            <div className="project-item">
+                                <div className="project-photo"><img src="./src/assets/images/projects/4.png" alt="" /></div>
+                                <h5>CREDIT RESERCH</h5>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 col-xs-12">
+                            <div className="project-item">
+                                <div className="project-photo"><img src="./src/assets/images/projects/5.png" alt="" /></div>
+                                <h5>TECHNICAL AID</h5>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 col-xs-12">
+                            <div className="project-item">
+                                <div className="project-photo"><img src="./src/assets/images/projects/6.png" alt="" /></div>
+                                <h5>TECHNICAL AID</h5>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 col-xs-12">
+                            <div className="project-item">
+                                <div className="project-photo"><img src="./src/assets/images/projects/7.png" alt="" /></div>
+                                <h5>TECHNICAL AID</h5>
+                            </div>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6 col-xs-12">
+                            <div className="project-item">
+                                <div className="project-photo"><img src="./src/assets/images/projects/8.png" alt="" /></div>
+                                <h5>TECHNICAL AID</h5>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default OurBusiness
---
+++ src/Components/OurFeatures.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+
+function OurFeatures() {
+  return (
+    <div>
+      <section id="our-feature" className="sections">
+            <div className="container">
+                <div className="heading">
+                    <div className="title text-center arrow-left">
+                        <img className="hidden-xs" src="./src/assets/images/left-arrow.png" alt="" />
+
+                        <h4 className="">Our Features</h4>
+                    </div>
+                </div>
+
+                <div className="feature-wrapper">
+                    <div className="row">
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content active"> 
+                                <h4><i className="fa fa-database"></i><span>Visual Composer</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Responsive</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 col-sm-6 col-xs-12">
+                            <div className="features-content"> 
+                                <h4><i className="fa fa-database"></i><span>Retina Ready</span></h4>
+                                <p>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulla</p>
+                            </div>
+                        </div>
+                    </div>	
+                </div>		
+
+
+            </div>
+
+
+        </section>
+    </div>
+  )
+}
+
+export default OurFeatures
